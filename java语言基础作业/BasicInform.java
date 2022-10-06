72d289ed0f71bd8ad2d7f32203031fb9f69c54d7
Tzj-721 <113233820+Tzj-721@users.noreply.github.com>
2022-10-06 23:59:42 +0800

    Create BasicInform.java

---
+++ java语言基础作业/BasicInform.java
@@ -0,0 +1,13 @@
+public class BasicInform {
+		    public static void main(String[] args) {
+		        String myName = "唐子剑";
+		        String sex = "男";
+		        String homeTown = "湖北省仙桃市";
+		        String address = "武汉大学";
+		        System.out.println("姓名：" + myName);
+		        System.out.println("性别：" + sex);
+		        System.out.println("籍贯：" + homeTown);
+		        System.out.println("地址：" + address);
+		    }
+		}
+
